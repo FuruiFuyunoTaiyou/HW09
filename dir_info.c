60793f5f2cac747181e4d991ec7256eb566df8e0
FuruiFuyunoTaiyou <wchen10@stuy.edu>
2017-11-03 04:15:02 -0400

    printing directories & other files separately

--- dir_info.c
+++ dir_info.c
@@ -3,41 +3,61 @@
 #include <string.h>
 #include <dirent.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <errno.h>
 
 int main(){
-  printf("Statistics for directory: \nFiles: \n");
-  DIR * directory = opendir(".");
+  printf("Statistics for directory . \n");
 
-  //printing all files
+  /*//printing all files
+  printf("Files: \n");
+  DIR * directory = opendir(".");
   struct dirent * entry = readdir(directory);
   while(entry){ //check for errors
-    
     printf("\t%s \n", entry->d_name);
     entry = readdir(directory);
   }
+  closedir(directory);*/
 
-  closedir(directory);
+  /*//finding total size of all files
+  printf("Total directory size: ");
+  int dir_size = 0;
+  directory = opendir(".");
+  entry = readdir(directory);
+  struct stat buff0;
+  stat(entry->d_name
+  while(entry){
+    dir_size += entry->
+  }
+  closedir(directory);*/
 
   //printing directories & other files separately
-  directory = opendir(".");
   printf("Directories: \n");
-  entry = readdir(directory);
-  while(entry && entry->d_type == DT_DIR){
-    printf("\t%s \n", entry->d_name);
+  DIR * directory = opendir(".");
+  struct dirent * entry = readdir(directory);
+  struct stat buff;
+  stat(entry->d_name, &buff);
+  while(entry){
+    if(S_ISDIR(buff.st_mode)){
+      printf("\t%s \n", entry->d_name);
+    }
     entry = readdir(directory);
+    stat(entry->d_name, &buff);
   }
-
   closedir(directory);
-  directory = opendir(".");
 
-  printf("Other Files: \n");
+  printf("Other: \n");
+  directory = opendir(".");
   entry = readdir(directory);
-  while(entry && entry->d_type != DT_DIR){
-    printf("\t%s \n", entry->d_name);
+  buff;
+  stat(entry->d_name, &buff);
+  while(entry){
+    if(!S_ISDIR(buff.st_mode)){
+      printf("\t%s \n", entry->d_name);
+    }
     entry = readdir(directory);
+    stat(entry->d_name, &buff);
   }
-  
   closedir(directory);
 
   return 0;
